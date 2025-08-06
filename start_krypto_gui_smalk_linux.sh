3e090a8d79b50633ac824f5035f2399de354dc9f
The220th <The220th@gmail.com>
2025-08-06 20:10:43 +0300

    starting scripts

---
+++ start_krypto_gui_smalk_linux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+VENV_DIR="venv"
+PACKAGE="krypto_gui_smalk"
+
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating venv..."
+    python3 -m venv $VENV_DIR
+    echo "Updating pip..."
+    . $VENV_DIR/bin/activate
+    pip install --upgrade pip
+    echo "Installing $PACKAGE..."
+    pip install $PACKAGE
+    deactivate
+else
+    echo "venv already exists."
+fi
+
+echo "Starting $PACKAGE..."
+source $VENV_DIR/bin/activate
+$PACKAGE
+deactivate
---
+++ start_krypto_gui_smalk_windows.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal
+
+set VENV_DIR=venv
+set PACKAGE=krypto_gui_smalk
+
+if not exist "%VENV_DIR%" (
+    echo Creating venv...
+    python -m venv %VENV_DIR%
+    echo Updating pip...
+    call %VENV_DIR%\Scripts\activate.bat
+    python -m pip install --upgrade pip
+    echo Installing %PACKAGE%...
+    python -m pip install %PACKAGE%
+    call deactivate
+) else (
+    echo venv already exists.
+)
+
+REM
+echo Starting %PACKAGE%...
+call %VENV_DIR%\Scripts\activate.bat
+krypto_gui_smalk
+call deactivate
+
+endlocal
